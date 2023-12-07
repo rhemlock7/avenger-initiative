582124bfc446fa4ea03142aa4a2bd17e2fe500eb
MMK <staph103@gmail.com>
2023-12-07 14:47:25 -0500

    worked on workout data and appended to the page

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,6 @@
 // Store HTML elements in variables
 var heroDetailDiv = document.querySelector('#hero-details')
-
+var workoutDetailsDiv = document.querySelector("#workout-details")
 var hulk = 'Iron Man'
 var captain = 'Captain America'
 
@@ -37,15 +37,56 @@ fetch(`https://gateway.marvel.com:443/v1/public/characters?name=${hulk}&apikey=8
 
 // https://api.api-ninjas.com/v1/exercises?muscle=
 // tFQ/n5S4oNe5c3vERyx93Q==960u5EA6MmusTREM
+muscle = "glutes"
 
-fetch('https://api.api-ninjas.com/v1/exercises?muscle=biceps', {
+fetch(`https://api.api-ninjas.com/v1/exercises?muscle=${muscle}`, {
     headers: {
-    "X-Api-Key": "tFQ/n5S4oNe5c3vERyx93Q==960u5EA6MmusTREM"
-    }})
-.then(res => res.json())
-.then(data => { 
-    console.log(data) })
+        "X-Api-Key": "tFQ/n5S4oNe5c3vERyx93Q==960u5EA6MmusTREM"
+    }
+})
+    .then(res => res.json())
+    .then(data => {
+
+        console.log(data)
+        
+   
+   
+        var workoutName = data[0].name;
+        var category = data[0].type;
+        var muscle = data[0].muscle;
+        var equipment = data[0].equipment;
+        var instr = data[0].instructions;
+
+        console.log(workoutName);
+        console.log(category);
+        console.log(muscle);
+        console.log(equipment);
+        console.log(instr);
+        
+        var workoutNameEl = document.createElement('li')
+        var categoryEl = document.createElement('li')
+        var muscleEl = document.createElement('li')
+        var equipmentEl = document.createElement('li')
+        var instrEl = document.createElement('li')
+
+        workoutNameEl.textContent = workoutName;
+        categoryEl.textContent = category;
+        muscleEl.textContent = muscle;
+        equipmentEl.textContent = equipment;
+        instrEl.textContent = instr;
+
+        workoutDetailsDiv.append(workoutNameEl)
+        workoutDetailsDiv.append(categoryEl)
+        workoutDetailsDiv.append(muscleEl)
+        workoutDetailsDiv.append(equipmentEl)
+        workoutDetailsDiv.append(instrEl)
+        console.log(workoutDetailsDiv);
+
+    })
     
+
+
+
 // #get both API's fetching data
 //*Marvel currently complete
 //TODO: Workout API
@@ -54,14 +95,63 @@ fetch('https://api.api-ninjas.com/v1/exercises?muscle=biceps', {
 //TODO: Need prevent default since it's a form
 //TODO: Function that fetches the API's by listening to if the value has changed within the selector?
 
-    heroselectformEl = document.querySelector("#hero-select-form")
+workoutSelectorEl = document.querySelector("#workouts")
+workoutSelectorEl.addEventListener('change', function () {
+    let choice = document.querySelector("#workout-dropdown")
+    console.log(choice)
+    if (choice.value === "chest") {
+        console.log(workoutName)
+    } else if (options.value === "biceps") {
+        console.log(muscle)
+
+    }else if(options.value === "glutes") {
+        console.log(instr)
+
+   }else if(options.value === "abdominals") {
+        console.log(category)
+   }
+})
+heroselectformEl = document.querySelector("#hero-select-form")
     heroselectformEl.addEventListener('change', function() {
         let options = document.querySelector("#hero-select-dropdown")
-        console.log(options)
+            console.log(options)
         if (options.value === "captain") {
             console.log("I am america")
         } else if (options.value === "hulk") {
             console.log("I am hulk")
-        } 
+        }
     })
-    
+
+
+        //     //loops through the data for the length of list in the data in incriments of 8 because the data shows every 3 hours
+        //     for (let index = 0; index < data.list.length; index = index + 8) {
+
+        //         //variable declarition for the 5 day forecast with index instead of 0 to increase by 8
+        //         var weatherDate = data.list[index].dt_txt;
+        //         var temp = data.list[index].main.temp;
+        //         var windInfo = data.list[index].wind.speed;
+        //         var humidity = data.list[index].main.humidity;
+        //         var iconCode = data.list[index].weather[0].icon;
+        //         var weatherIcon = fIcon + iconCode + "@2x.png";
+
+        //         console.log(weatherDate);
+        //         var cardInfo = $(`<li class="card col-lg-2 m-2"></li>`);
+        //         var h4El = $("<h4>");
+        //         h4El.text(weatherDate)
+        //         var weatherIconEl = $('<img>').attr('src', weatherIcon);
+        //         var tempEl = $('<p>').text("Temp: " + temp + " °F")
+        //         var windInfoEl = $('<p>').text("Wind: " + windInfo + " MPH")
+        //         var humidityEl = $('<p>').text("Humidity: " + humidity + " %")
+        //         var cardContainer = $('.info');
+        //         var cardDiv = $('<li>');
+
+        //         //appends created elements to the page 
+        //         cardInfo.append(h4El);
+        //         cardInfo.append(weatherIconEl);
+        //         cardInfo.append(tempEl);
+        //         cardInfo.append(windInfoEl);
+        //         cardInfo.append(humidityEl);
+        //         cardSlot.append(cardInfo);
+        //     }
+
+        // });
