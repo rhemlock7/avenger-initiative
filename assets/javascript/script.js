41c33d67f17fc75f431791af498f1bd190630fdc
Ryan Hemlock <ryan@ryanhemlock.com>
2023-12-07 15:33:00 -0500

    Hero data now persists upon page load by being store in local storage

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -9,14 +9,32 @@ var weeklyViewContainer = document.querySelector('#weekly-container');
 
 
 // Event Listener on hero dropdown
-
 heroselectformEl = document.querySelector("#hero-select-form")
 heroselectformEl.addEventListener('change', function (event) {
 
     let hero = event.target.value;
     console.log(hero);
 
-    fetch(`https://gateway.marvel.com:443/v1/public/characters?name=${hero}&apikey=851e0da0c6b577d3681246bac28477e8`)
+    localStorage.setItem("hero", hero);
+
+    getHeroInfo(hero)
+
+})
+
+// Function that takes the hero data and displays it on screen
+function getHeroInfo(input) {
+    let storedHero = localStorage.getItem("hero");
+
+    let localHero;
+
+    if (input) {
+        localHero = input;
+    } else {
+        localHero = storedHero;
+    }
+
+    if (storedHero != null) {
+        fetch(`https://gateway.marvel.com:443/v1/public/characters?name=${localHero}&apikey=851e0da0c6b577d3681246bac28477e8`)
         .then(res => res.json())
         .then(data => {
             console.log(data);
@@ -55,136 +73,86 @@ heroselectformEl.addEventListener('change', function (event) {
                 displayHero();
             }
 
-            createWeeklyView(hero)
+            createWeeklyView(localHero)
         })
-})
-
-
-
-
-
-
-
-// https://api.api-ninjas.com/v1/exercises?muscle=
-// tFQ/n5S4oNe5c3vERyx93Q==960u5EA6MmusTREM
-muscle = "glutes"
-
-fetch(`https://api.api-ninjas.com/v1/exercises?muscle=${muscle}`, {
-    headers: {
-        "X-Api-Key": "tFQ/n5S4oNe5c3vERyx93Q==960u5EA6MmusTREM"
     }
-})
-    .then(res => res.json())
-    .then(data => {
 
-        console.log(data)
-        
-   
-   
-        var workoutName = data[0].name;
-        var category = data[0].type;
-        var muscle = data[0].muscle;
-        var equipment = data[0].equipment;
-        var instr = data[0].instructions;
-
-        console.log(workoutName);
-        console.log(category);
-        console.log(muscle);
-        console.log(equipment);
-        console.log(instr);
-        
-        var workoutNameEl = document.createElement('li')
-        var categoryEl = document.createElement('li')
-        var muscleEl = document.createElement('li')
-        var equipmentEl = document.createElement('li')
-        var instrEl = document.createElement('li')
-
-        workoutNameEl.textContent = workoutName;
-        categoryEl.textContent = category;
-        muscleEl.textContent = muscle;
-        equipmentEl.textContent = equipment;
-        instrEl.textContent = instr;
-
-        workoutDetailsDiv.append(workoutNameEl)
-        workoutDetailsDiv.append(categoryEl)
-        workoutDetailsDiv.append(muscleEl)
-        workoutDetailsDiv.append(equipmentEl)
-        workoutDetailsDiv.append(instrEl)
-        console.log(workoutDetailsDiv);
-
-    })
     
+}
 
 
 
-// #get both API's fetching data
-//*Marvel currently complete
-//TODO: Workout API
 
-// #Add event listening on form
-//TODO: Need prevent default since it's a form
-//TODO: Function that fetches the API's by listening to if the value has changed within the selector?
+// TODO --------------------------------------------------------------------------------
+// Mustapha's workout data fetch being displayed in Daily View Container
 
+// muscle = "glutes"
+
+// fetch(`https://api.api-ninjas.com/v1/exercises?muscle=${muscle}`, {
+//     headers: {
+//         "X-Api-Key": "tFQ/n5S4oNe5c3vERyx93Q==960u5EA6MmusTREM"
+//     }
+// })
+//     .then(res => res.json())
+//     .then(data => {
+
+//         console.log(data)
+
+
+
+//         var workoutName = data[0].name;
+//         var category = data[0].type;
+//         var muscle = data[0].muscle;
+//         var equipment = data[0].equipment;
+//         var instr = data[0].instructions;
+
+//         console.log(workoutName);
+//         console.log(category);
+//         console.log(muscle);
+//         console.log(equipment);
+//         console.log(instr);
+
+//         var workoutNameEl = document.createElement('li')
+//         var categoryEl = document.createElement('li')
+//         var muscleEl = document.createElement('li')
+//         var equipmentEl = document.createElement('li')
+//         var instrEl = document.createElement('li')
+
+//         workoutNameEl.textContent = workoutName;
+//         categoryEl.textContent = category;
+//         muscleEl.textContent = muscle;
+//         equipmentEl.textContent = equipment;
+//         instrEl.textContent = instr;
+
+//         workoutDetailsDiv.append(workoutNameEl)
+//         workoutDetailsDiv.append(categoryEl)
+//         workoutDetailsDiv.append(muscleEl)
+//         workoutDetailsDiv.append(equipmentEl)
+//         workoutDetailsDiv.append(instrEl)
+//         console.log(workoutDetailsDiv);
+//     })
+// TODO --------------------------------------------------------------------------------
+
+//Event Listener for filtering workouts -- Working
 workoutSelectorEl = document.querySelector("#workouts")
 workoutSelectorEl.addEventListener('change', function () {
     let choice = document.querySelector("#workout-dropdown")
     console.log(choice)
     if (choice.value === "chest") {
-        console.log(workoutName)
+        console.log("chest")
     } else if (options.value === "biceps") {
-        console.log(muscle)
+        console.log("biceps")
 
-    }else if(options.value === "glutes") {
-        console.log(instr)
+    } else if (options.value === "glutes") {
+        console.log("glutes")
 
-   }else if(options.value === "abdominals") {
+    } else if (options.value === "abdominals") {
         console.log(category)
-   }
+    }
 })
-heroselectformEl = document.querySelector("#hero-select-form")
-    heroselectformEl.addEventListener('change', function() {
-        let options = document.querySelector("#hero-select-dropdown")
-            console.log(options)
-        if (options.value === "captain") {
-            console.log("I am america")
-        } else if (options.value === "hulk") {
-            console.log("I am hulk")
-        }
-    })
 
 
-        //     //loops through the data for the length of list in the data in incriments of 8 because the data shows every 3 hours
-        //     for (let index = 0; index < data.list.length; index = index + 8) {
 
-        //         //variable declarition for the 5 day forecast with index instead of 0 to increase by 8
-        //         var weatherDate = data.list[index].dt_txt;
-        //         var temp = data.list[index].main.temp;
-        //         var windInfo = data.list[index].wind.speed;
-        //         var humidity = data.list[index].main.humidity;
-        //         var iconCode = data.list[index].weather[0].icon;
-        //         var weatherIcon = fIcon + iconCode + "@2x.png";
-
-        //         console.log(weatherDate);
-        //         var cardInfo = $(`<li class="card col-lg-2 m-2"></li>`);
-        //         var h4El = $("<h4>");
-        //         h4El.text(weatherDate)
-        //         var weatherIconEl = $('<img>').attr('src', weatherIcon);
-        //         var tempEl = $('<p>').text("Temp: " + temp + " °F")
-        //         var windInfoEl = $('<p>').text("Wind: " + windInfo + " MPH")
-        //         var humidityEl = $('<p>').text("Humidity: " + humidity + " %")
-        //         var cardContainer = $('.info');
-        //         var cardDiv = $('<li>');
-
-        //         //appends created elements to the page 
-        //         cardInfo.append(h4El);
-        //         cardInfo.append(weatherIconEl);
-        //         cardInfo.append(tempEl);
-        //         cardInfo.append(windInfoEl);
-        //         cardInfo.append(humidityEl);
-        //         cardSlot.append(cardInfo);
-        //     }
-
-        // });
 function createWeeklyView(heroValue) {
     var weeklyWorkoutContainer = document.createElement('div');
     weeklyWorkoutContainer.setAttribute("id", "workout-weekly-container")
@@ -225,3 +193,6 @@ function createWeeklyView(heroValue) {
 
     //weeklyViewContainer
 }
+
+// Calls function immediately to get hero from local storage
+getHeroInfo()
