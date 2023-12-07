edc72a8bb69f9c8a37df648b903f67064c4a2ad8
Ryan Hemlock <ryan@ryanhemlock.com>
2023-12-07 11:44:44 -0500

    Now fetching correct Marvel data

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,12 +1,20 @@
 var hulk = 'Hulk'
-fetch(`https://gateway.marvel.com:443/v1/public/characters?name=${hulk}&apikey=851e0da0c6b577d3681246bac28477e8`)
+var Captain = 'Captain America'
+
+fetch(`https://gateway.marvel.com:443/v1/public/characters?name=${Captain}&apikey=851e0da0c6b577d3681246bac28477e8`)
 .then(res => res.json())
-.then(data => { console.log(marvelData); })
+.then(data => { 
+    console.log(data); 
+    console.log(data.data.results[0])
+    console.log("Name: " + data.data.results[0].name)
+    console.log("Image Link: " + data.data.results[0].thumbnail.path + "." + data.data.results[0].thumbnail.extension)
+    console.log("Description: " + data.data.results[0].description)
+})
 
 
-fetch('apikey=cc4333db4938d18f524710563c07f5b9843cfbc7')
-.then(res => res.json())
-.then(data => { console.log(workoutData) })
+// fetch('apikey=cc4333db4938d18f524710563c07f5b9843cfbc7')
+// .then(res => res.json())
+// .then(data => { console.log(workoutData) })
 
 // #get both API's fetching data
     //*Marvel currently complete
@@ -17,3 +25,8 @@ fetch('apikey=cc4333db4938d18f524710563c07f5b9843cfbc7')
     //TODO: Function that fetches the API's by listening to if the value has changed within the selector?
 
 // #
+
+// Hero Details to display on screen:
+// 1. Hero Image
+// 2. Hero name
+// 3. Hero Description
