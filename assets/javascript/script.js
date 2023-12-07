021d660c05436835db180c46e61bc4f5e3bac1e0
Tyler-Cav <Tyler.Cavanaugh24@gmail.com>
2023-12-07 11:42:50 -0500

    Updated within partial working event listener

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,4 +6,23 @@
     //TODO: Need prevent default since it's a form
     //TODO: Function that fetches the API's by listening to if the value has changed within the selector?
 
-// #
+
+
+    // function HeroSeleted(e) {
+    //     e.preventDefault()
+    //     console.log(HeroSeleted)
+    // }
+    heroselectformEl = document.querySelector("#hero-select-form")
+    console.log(heroselectformEl)
+
+    heroselectformEl.addEventListener('change', function(event) {
+        let options = document.querySelector("#hero-select-dropdown")
+        if (options.children[1].innerHTML === "Captain America") {
+            console.log("I am america")
+
+        } else if (options.children[2].innerHTML === "Hulk") {
+            console.log("I am hulk")
+        }
+    })
+    
+    
