ddccdddd6a31c9dc2d24c5b5500dd0cec1c13fb0
MMK <staph103@gmail.com>
2023-12-07 17:00:34 -0500

    functional workout display

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -59,132 +59,89 @@ heroselectformEl.addEventListener('change', function (event) {
         })
 })
 
+//MUSTAPHA: fetches the workout api and responds with a JSON 
+workoutsEl = document.querySelector("#workouts")
+workoutsEl.addEventListener('change', function (event) {
+
+    let muscle = event.target.value;
+    console.log(muscle);
 
 
-
-
-
-
-// https://api.api-ninjas.com/v1/exercises?muscle=
-// tFQ/n5S4oNe5c3vERyx93Q==960u5EA6MmusTREM
-muscle = "glutes"
-
-fetch(`https://api.api-ninjas.com/v1/exercises?muscle=${muscle}`, {
-    headers: {
-        "X-Api-Key": "tFQ/n5S4oNe5c3vERyx93Q==960u5EA6MmusTREM"
-    }
-})
-    .then(res => res.json())
-    .then(data => {
-
-        console.log(data)
-        
-   
-   
-        var workoutName = data[0].name;
-        var category = data[0].type;
-        var muscle = data[0].muscle;
-        var equipment = data[0].equipment;
-        var instr = data[0].instructions;
-
-        console.log(workoutName);
-        console.log(category);
-        console.log(muscle);
-        console.log(equipment);
-        console.log(instr);
-        
-        var workoutNameEl = document.createElement('li')
-        var categoryEl = document.createElement('li')
-        var muscleEl = document.createElement('li')
-        var equipmentEl = document.createElement('li')
-        var instrEl = document.createElement('li')
-
-        workoutNameEl.textContent = workoutName;
-        categoryEl.textContent = category;
-        muscleEl.textContent = muscle;
-        equipmentEl.textContent = equipment;
-        instrEl.textContent = instr;
-
-        workoutDetailsDiv.append(workoutNameEl)
-        workoutDetailsDiv.append(categoryEl)
-        workoutDetailsDiv.append(muscleEl)
-        workoutDetailsDiv.append(equipmentEl)
-        workoutDetailsDiv.append(instrEl)
-        console.log(workoutDetailsDiv);
-
-    })
-    
-
-
-
-// #get both API's fetching data
-//*Marvel currently complete
-//TODO: Workout API
-
-// #Add event listening on form
-//TODO: Need prevent default since it's a form
-//TODO: Function that fetches the API's by listening to if the value has changed within the selector?
-
-workoutSelectorEl = document.querySelector("#workouts")
-workoutSelectorEl.addEventListener('change', function () {
-    let choice = document.querySelector("#workout-dropdown")
-    console.log(choice)
-    if (choice.value === "chest") {
-        console.log(workoutName)
-    } else if (options.value === "biceps") {
-        console.log(muscle)
-
-    }else if(options.value === "glutes") {
-        console.log(instr)
-
-   }else if(options.value === "abdominals") {
-        console.log(category)
-   }
-})
-heroselectformEl = document.querySelector("#hero-select-form")
-    heroselectformEl.addEventListener('change', function() {
-        let options = document.querySelector("#hero-select-dropdown")
-            console.log(options)
-        if (options.value === "captain") {
-            console.log("I am america")
-        } else if (options.value === "hulk") {
-            console.log("I am hulk")
+    fetch(`https://api.api-ninjas.com/v1/exercises?muscle=${muscle}`, {
+        headers: {
+            "X-Api-Key": "tFQ/n5S4oNe5c3vERyx93Q==960u5EA6MmusTREM"
         }
     })
+        .then(res => res.json())
+        .then(data => {
+
+            console.log(data)
 
 
-        //     //loops through the data for the length of list in the data in incriments of 8 because the data shows every 3 hours
-        //     for (let index = 0; index < data.list.length; index = index + 8) {
+//MUSTAPHA: created function for workouts to collect data and display in Daily View container
+            function showsWorkouts() {
 
-        //         //variable declarition for the 5 day forecast with index instead of 0 to increase by 8
-        //         var weatherDate = data.list[index].dt_txt;
-        //         var temp = data.list[index].main.temp;
-        //         var windInfo = data.list[index].wind.speed;
-        //         var humidity = data.list[index].main.humidity;
-        //         var iconCode = data.list[index].weather[0].icon;
-        //         var weatherIcon = fIcon + iconCode + "@2x.png";
+                var i = 0
+                var workoutName = data[i].name;
 
-        //         console.log(weatherDate);
-        //         var cardInfo = $(`<li class="card col-lg-2 m-2"></li>`);
-        //         var h4El = $("<h4>");
-        //         h4El.text(weatherDate)
-        //         var weatherIconEl = $('<img>').attr('src', weatherIcon);
-        //         var tempEl = $('<p>').text("Temp: " + temp + " °F")
-        //         var windInfoEl = $('<p>').text("Wind: " + windInfo + " MPH")
-        //         var humidityEl = $('<p>').text("Humidity: " + humidity + " %")
-        //         var cardContainer = $('.info');
-        //         var cardDiv = $('<li>');
+                var category = data[i].type;
+                var muscle = data[i].muscle;
+                var equipment = data[i].equipment;
+                var instr = data[i].instructions;
+                console.log(workoutName);
+                console.log(category);
+                console.log(muscle);
+                console.log(equipment);
+                console.log(instr);
+//MUSTAPHA: creates elements for retrieved data
+                var workoutNameEl = document.createElement('li')
+                var categoryEl = document.createElement('li')
+                var muscleEl = document.createElement('li')
+                var equipmentEl = document.createElement('li')
+                var instrEl = document.createElement('li')
+                instrEl.setAttribute("style", "font-size: 10px")
+                workoutNameEl.textContent = "Workout - " + workoutName;
+                categoryEl.textContent = "Type - " + category;
+                muscleEl.textContent = "Muscle - " + muscle;
+                equipmentEl.textContent = "Equipment - " + equipment;
+                instrEl.textContent = instr;
+//MUSTAPHA:appends the created elements to the html
+                workoutDetailsDiv.append(workoutNameEl)
+                workoutDetailsDiv.append(categoryEl)
+                workoutDetailsDiv.append(muscleEl)
+                workoutDetailsDiv.append(equipmentEl)
+                workoutDetailsDiv.append(instrEl)
+                console.log(workoutDetailsDiv);
+            }
 
-        //         //appends created elements to the page 
-        //         cardInfo.append(h4El);
-        //         cardInfo.append(weatherIconEl);
-        //         cardInfo.append(tempEl);
-        //         cardInfo.append(windInfoEl);
-        //         cardInfo.append(humidityEl);
-        //         cardSlot.append(cardInfo);
-        //     }
+//MUSTAPHA:removes all children before display new children
+            if (workoutDetailsDiv.hasChildNodes()) {
+                // Loop backward to remove all children
+                for (let i = workoutDetailsDiv.children.length - 1; i >= 0; i--) {
+                    workoutDetailsDiv.removeChild(workoutDetailsDiv.children[i]);
+                }
 
-        // });
+                showsWorkouts();
+
+            } else {
+
+                showsWorkouts();
+            }
+
+
+        })
+
+})
+
+
+
+
+// var nextBtn = document.querySelector('#next');
+// nextBtn.addEventListener("click", nextBtnFunction)
+
+
+
+
 function createWeeklyView(heroValue) {
     var weeklyWorkoutContainer = document.createElement('div');
     weeklyWorkoutContainer.setAttribute("id", "workout-weekly-container")
