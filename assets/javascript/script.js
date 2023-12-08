45b45994e607cecc4a2a4cdb2c06ab726eab2ebc
MMK <staph103@gmail.com>
2023-12-08 11:33:26 -0500

    fixed workout section and added prev and next btns

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,8 +5,9 @@ var hulk = 'Iron Man'
 var captain = 'Captain America'
 var heroDetailDiv = document.querySelector('#hero-details');
 var weeklyViewContainer = document.querySelector('#weekly-container');
-
-
+var workoutIndex = 0;
+var workoutData;
+var difficulty = [];
 
 // Event Listener on hero dropdown
 
@@ -64,10 +65,11 @@ workoutsEl = document.querySelector("#workouts")
 workoutsEl.addEventListener('change', function (event) {
 
     let muscle = event.target.value;
+    // let difficulty = 'difficulty='+ "expert&";
     console.log(muscle);
 
 
-    fetch(`https://api.api-ninjas.com/v1/exercises?muscle=${muscle}`, {
+    fetch(`https://api.api-ninjas.com/v1/exercises?${difficulty}muscle=${muscle}`, {
         headers: {
             "X-Api-Key": "tFQ/n5S4oNe5c3vERyx93Q==960u5EA6MmusTREM"
         }
@@ -76,68 +78,96 @@ workoutsEl.addEventListener('change', function (event) {
         .then(data => {
 
             console.log(data)
-
-
-//MUSTAPHA: created function for workouts to collect data and display in Daily View container
-            function showsWorkouts() {
-
-                var i = 0
-                var workoutName = data[i].name;
-
-                var category = data[i].type;
-                var muscle = data[i].muscle;
-                var equipment = data[i].equipment;
-                var instr = data[i].instructions;
-                console.log(workoutName);
-                console.log(category);
-                console.log(muscle);
-                console.log(equipment);
-                console.log(instr);
-//MUSTAPHA: creates elements for retrieved data
-                var workoutNameEl = document.createElement('li')
-                var categoryEl = document.createElement('li')
-                var muscleEl = document.createElement('li')
-                var equipmentEl = document.createElement('li')
-                var instrEl = document.createElement('li')
-                instrEl.setAttribute("style", "font-size: 10px")
-                workoutNameEl.textContent = "Workout - " + workoutName;
-                categoryEl.textContent = "Type - " + category;
-                muscleEl.textContent = "Muscle - " + muscle;
-                equipmentEl.textContent = "Equipment - " + equipment;
-                instrEl.textContent = instr;
-//MUSTAPHA:appends the created elements to the html
-                workoutDetailsDiv.append(workoutNameEl)
-                workoutDetailsDiv.append(categoryEl)
-                workoutDetailsDiv.append(muscleEl)
-                workoutDetailsDiv.append(equipmentEl)
-                workoutDetailsDiv.append(instrEl)
-                console.log(workoutDetailsDiv);
-            }
-
-//MUSTAPHA:removes all children before display new children
-            if (workoutDetailsDiv.hasChildNodes()) {
-                // Loop backward to remove all children
-                for (let i = workoutDetailsDiv.children.length - 1; i >= 0; i--) {
-                    workoutDetailsDiv.removeChild(workoutDetailsDiv.children[i]);
-                }
-
-                showsWorkouts();
-
-            } else {
-
-                showsWorkouts();
-            }
-
+            workoutData = data;
+            showsWorkouts();
 
         })
 
 })
+function nextBtnFunction() {
+    if (workoutIndex < 6) {
+        prevBtn.classList.remove('diable-btn')
+        workoutIndex++;
+        showsWorkouts(workoutIndex);
+        console.log("you clicked me ")
+        prevBtn.classList.remove('hide')
+        console.log(workoutIndex)
+
+        if (workoutIndex === 6) {
+            nextBtn.classList.add('diable-btn')
+        }
+    }
+
+}
+function prevBtnFunction() {
+    if (workoutIndex > 0) {
+        nextBtn.classList.remove('diable-btn')
+        workoutIndex--;
+        showsWorkouts(workoutIndex);
+        console.log("you clicked prev ")
+        prevBtn.classList.remove('diable-btn')
+        console.log(workoutIndex)
+
+        if (workoutIndex === 0) {
+            prevBtn.classList.add('diable-btn')
+        }
+    }
 
 
 
+}
 
-// var nextBtn = document.querySelector('#next');
-// nextBtn.addEventListener("click", nextBtnFunction)
+function showsWorkouts() {
+
+    //removes hide from next buttom
+    nextBtn.classList.remove('hide')
+    //MUSTAPHA:removes all children before display new children
+    if (workoutDetailsDiv.hasChildNodes()) {
+        for (let i = workoutDetailsDiv.children.length - 1; i >= 0; i--) {
+            workoutDetailsDiv.removeChild(workoutDetailsDiv.children[i]);
+        }
+
+    }
+    //MUSTAPHA: created function for workouts to collect data and display in Daily View container
+    var workoutName = workoutData[workoutIndex].name;
+    var category = workoutData[workoutIndex].type;
+    var muscle = workoutData[workoutIndex].muscle;
+    var equipment = workoutData[workoutIndex].equipment;
+    var instr = workoutData[workoutIndex].instructions;
+    console.log(workoutName);
+    console.log(category);
+    console.log(muscle);
+    console.log(equipment);
+    console.log(instr);
+
+    //MUSTAPHA: creates elements for retrieved data
+    var workoutNameEl = document.createElement('li')
+    var categoryEl = document.createElement('li')
+    var muscleEl = document.createElement('li')
+    var equipmentEl = document.createElement('li')
+    var instrEl = document.createElement('li')
+    instrEl.setAttribute("style", "font-size: 10px")
+    workoutNameEl.textContent = "Workout - " + workoutName;
+    categoryEl.textContent = "Type - " + category;
+    muscleEl.textContent = "Muscle - " + muscle;
+    equipmentEl.textContent = "Equipment - " + equipment;
+    instrEl.textContent = instr;
+    //MUSTAPHA:appends the created elements to the html
+    workoutDetailsDiv.append(workoutNameEl)
+    workoutDetailsDiv.append(categoryEl)
+    workoutDetailsDiv.append(muscleEl)
+    workoutDetailsDiv.append(equipmentEl)
+    workoutDetailsDiv.append(instrEl)
+    console.log(workoutDetailsDiv);
+}
+
+
+
+//creates variables for event listener
+var nextBtn = document.querySelector('#next');
+var prevBtn = document.querySelector('#prev');
+nextBtn.addEventListener("click", nextBtnFunction);
+prevBtn.addEventListener("click", prevBtnFunction)
 
 
 
